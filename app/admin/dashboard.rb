966e420269aee7aa44b8d4ed802d6b8a90b39899
wu junlin <welcometeddy@yahoo.com.tw>
2018-08-17 12:46:06 +0800

    修改中文

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,30 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+      # span class: "blank_slate" do
+        # span I18n.t("active_admin.dashboard_welcome.welcome")
+        # small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      # end
+    # end
+
+    columns do
+      column do
+        panel "最近的訂單" do
+          table_for Order.order("id desc").limit(10) do
+            column("Email") { |order| order.supporter.email }
+            column("數量") { |order| order.quantity }
+            column("金額")   { |order| order.amount }
+          end
+        end
+      end
+      column do
+        div do
+          br
+          text_node %{<iframe src="https://rpm.newrelic.com/public/charts/3qK14CnDWRB"
+                              width="500" height="300" scrolling="no" frameborder="no">
+                      </iframe>}.html_safe
+        end
       end
     end
 
