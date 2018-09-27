521e2187bedee767df4b9bc6338c29feeb3d5219
wu junlin <welcometeddy@yahoo.com.tw>
2018-09-27 17:44:19 +0800

    移除測試碼

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -8,4 +8,23 @@ module OrdersHelper
   	end
   end
 
+  def render_how_to_pay(payment_type)
+  	case payment_type
+  	when "Credit_CreditCard"
+  		"信用卡"
+  	when "BARCODE_BARCODE"
+  		"7-11 ibon 代碼繳款"
+  	when "CVS_CVS" 
+  		"超商代碼繳款"
+		when "CVS_OK" 
+			"OK 超商代碼繳款"
+		when "CVS_FAMILY" 
+			"全家超商代碼繳款"
+		when "CVS_HILIFE" 
+			"萊爾富超商代碼繳款"
+		when "GooglePay"
+			"GooglePay"
+	  end
+  end
+
 end
