0cf13f657e371a2c4cc4116598b740c80f6155e9
wu junlin <welcometeddy@yahoo.com.tw>
2018-10-18 19:30:43 +0800

    調整修正

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -57,6 +57,10 @@ class User::RegistrationsController < Devise::RegistrationsController
     params.require(:user).permit(:nickname)
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
