70f5df53bfeebfbcd6b6f338b3f429664a09493c
wu junlin <welcometeddy@yahoo.com.tw>
2018-12-07 16:25:09 +0800

    domain調整

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 custom = YAML.load_file("config/settings.yml")
-Crowdfunding::Application.config.session_store :cookie_store, key: '_ugooz_session', domain: :custom["domain"]
+Crowdfunding::Application.config.session_store :cookie_store, key: '_ugooz_session', domain: custom[:domain]
