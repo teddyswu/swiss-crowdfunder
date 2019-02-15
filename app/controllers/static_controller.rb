e569eec1b5769bd86f63ba1b2adf09787014325c
wu junlin <welcometeddy@yahoo.com.tw>
2019-02-15 13:02:31 +0800

    修正

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,8 +2,7 @@ class StaticController < ApplicationController
   http_basic_authenticate_with name: "letme", password: "in", only: :exception_test
 
   def about_us
-    set_page_description("手機王(首機網路) 近20年來默默善盡企業社會責任，積極扮演企業公民的角色，從高海拔的部落到偏遠的鄉鎮都有我們的足跡。
-我們希望透過友故事，聚集更多的友善小農和支持者，將更多的理念和故事，傳遞給大家。與友善小農交朋友，鼓勵他們努力種出更優質、對環境更友善的作物。")
+    set_page_description("手機王(首機網路) 近20年來默默善盡企業社會責任，積極扮演企業公民的角色。透過友故事，聚集更多友善小農和支持者，將更多的理念和故事，傳遞給大家。與友善小農交朋友，鼓勵他們努力種出更優質、對環境更友善的作物。")
     set_page_image "https://www.ugooz.cc/assets/sk2/img/ui/logo.png"
     set_page_title "關於我們"
   end
@@ -15,7 +14,7 @@ class StaticController < ApplicationController
   end
 
   def privacy_policy
-    set_page_description("隱私權政策    一、本政策適用範圍:  1. 本隱私權政策適用於您在本網站活動時，所涉及的個人資料蒐集、運用與保護，包含：使用者名稱、年齡、性別、職業、通訊地址、 Email...")
+    set_page_description("一、本政策適用範圍:  1. 本隱私權政策適用於您在本網站活動時，所涉及的個人資料蒐集、運用與保護，包含：使用者名稱、年齡、性別、職業、通訊地址、 Email...")
     set_page_image "https://www.ugooz.cc/assets/sk2/img/ui/logo.png"
     set_page_title "隱私權政策"
   end
