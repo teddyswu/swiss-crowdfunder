84e38c956758102e2a492fa511c1c87f8c825cb8
wu junlin <welcometeddy@yahoo.com.tw>
2019-05-22 17:48:21 +0800

    調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:is_paid]
+  skip_before_action :verify_authenticity_token, only: [:is_paid, :payment_info]
   before_action :authenticate_user!, only: [:new, :index, :detail]
 
   # TODO: Think about proper safety in this method/controller. This
