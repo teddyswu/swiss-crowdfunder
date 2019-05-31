595bcc4655f7afcd65d78ae3731b35a03dbe9ea2
wu junlin <welcometeddy@yahoo.com.tw>
2019-05-31 11:37:50 +0800

    設定時區

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Crowdfunding
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.exceptions_app = self.routes
+    config.time_zone = 'Asia/Taipei'
     config.active_job.queue_adapter = :delayed_job
     
   end
