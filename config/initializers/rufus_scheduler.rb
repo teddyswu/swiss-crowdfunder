c6cb072f6436c034c25ee4f70a1751dcc77a39ff
wu junlin <welcometeddy@yahoo.com.tw>
2019-06-04 22:12:20 +0800

    改回來

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -21,7 +21,7 @@ end
 safely_and_compute_time do
   scheduler  = Rufus::Scheduler.start_new
   proc_mutex = Mutex.new # 初始化一個 process 鎖  
-  scheduler.cron '20 14 * * *', :mutex => proc_mutex do
+  scheduler.cron '00 01 * * *', :mutex => proc_mutex do
     camps = Campaign.where(:result_status => nil).to_a#.where("end_date < ?", Date.today)
     camps.each do |camp|
       File.open("#{Rails.root}/log/jobs.log", "a+") do |file|
