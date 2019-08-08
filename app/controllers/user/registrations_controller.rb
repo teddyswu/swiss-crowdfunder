c3c1cdccd9e1b8547ccae43ea79b1227efcf5214
wu junlin <welcometeddy@yahoo.com.tw>
2019-08-08 16:58:15 +0800

    調整註冊頁面

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -25,16 +25,41 @@ class User::RegistrationsController < Devise::RegistrationsController
     end
     UserProfile.find_or_create_by(:user_id => resource.id)
     set_page_title "帳號管理"
-    
     super
   end
 
   # PUT /resource
   def update
-    super
+    #==========
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+    resource_updated = update_resource(resource, account_update_params)
     resource.update_attributes!(user_params)
     resource.user_profile.update_attributes!(user_profile_params)
     set_flash_message! :notice, :updated
+
+    yield resource if block_given?
+    if resource_updated
+      if is_flashing_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      bypass_sign_in resource, scope: resource_name
+      p params[:s] != ""
+      p params[:s]
+      p "================"
+      if params[:s] != ""
+        redirect_to "https://m.me/agrisogi?ref=u_advisory" and return
+      end
+      respond_with resource, location: after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+    #==========
   end
 
   # DELETE /resource
@@ -71,7 +96,7 @@ class User::RegistrationsController < Devise::RegistrationsController
   end
 
   def after_update_path_for(resource)
-    edit_user_registration_path(resource)
+    edit_user_registration_path
   end
 
   # If you have extra params to permit, append them to the sanitizer.
